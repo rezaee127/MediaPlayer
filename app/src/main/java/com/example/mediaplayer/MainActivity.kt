2e7b49fd9cb1403212bda10e3c073c12004aeb15
razi <rezaie127@yahoo.com>
2022-04-22 12:01:28 +0430

    added button

--- app/src/main/java/com/example/mediaplayer/MainActivity.kt
+++ app/src/main/java/com/example/mediaplayer/MainActivity.kt
@@ -28,31 +28,14 @@ class MainActivity : AppCompatActivity() {
     var isMusicPlaying = false
     lateinit var binding: ActivityMainBinding
     var mediaPlayer: MediaPlayer? = null
-    private var fileName: String = ""
-    private val requestPermissionLauncher =
-        registerForActivityResult(
-            ActivityResultContracts.RequestPermission()
-        ) { isGranted: Boolean ->
-            if (isGranted) {
-                onRecord(true)
-            } else {
-            }
-        }
 
-    private var recordButton: RecordButton? = null
-    private var recorder: MediaRecorder? = null
-    private var playButton: PlayButton? = null
-    private var player: MediaPlayer? = null
 
-    // Requesting permission to RECORD_AUDIO
-    private var permissionToRecordAccepted = false
-    private var permissions: Array<String> = arrayOf(Manifest.permission.RECORD_AUDIO)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        create2()
+
         initViews()
 
 
@@ -92,192 +75,12 @@ class MainActivity : AppCompatActivity() {
         }
 
         binding.buttonStartRecord.setOnClickListener {
-            requestPermissions()
-//            onRecord(true)
+
         }
-        binding.buttonStopRecord.setOnClickListener { onRecord(false) }
-    }
-
-
-    private fun requestPermissions() {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            when {
-                //if user already granted the permission
-                ContextCompat.checkSelfPermission(
-                    this,
-                    Manifest.permission.RECORD_AUDIO
-                ) == PackageManager.PERMISSION_GRANTED -> {
-                    onRecord(true)
-                }
-                //if user already denied the permission once
-                ActivityCompat.shouldShowRequestPermissionRationale(
-                    this,
-                    Manifest.permission.RECORD_AUDIO
-                ) -> {
-                    showRationDialog()
-                }
-                else -> {
-                    requestPermissionLauncher.launch(
-                        Manifest.permission.RECORD_AUDIO
-                    )
-                }
-            }
-        }
-    }
-
-    private fun showRationDialog() {
-        val builder = AlertDialog.Builder(this)
-        builder.apply {
-            setMessage("we need microphone")
-            setTitle("permission required")
-            setPositiveButton("ok") { dialog, which ->
-                requestPermissionLauncher.launch(
-                    Manifest.permission.RECORD_AUDIO,
-                )
-            }
-        }
-        builder.create().show()
-    }
-
-    //    override fun onRequestPermissionsResult(
-//        requestCode: Int,
-//        permissions: Array<String>,
-//        grantResults: IntArray
-//    ) {
-//        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-//        permissionToRecordAccepted = if (requestCode == REQUEST_RECORD_AUDIO_PERMISSION) {
-//            grantResults[0] == PackageManager.PERMISSION_GRANTED
-//        } else {
-//            false
-//        }
-//        if (!permissionToRecordAccepted) finish()
-//    }
-    private fun onPlay(start: Boolean) = if (start) {
-        startPlaying()
-    } else {
-        stopPlaying()
-    }
-    private fun startPlaying() {
-        player = MediaPlayer().apply {
-            try {
-                setDataSource(fileName)
-                prepare()
-                start()
-            } catch (e: IOException) {
-                Log.e(LOG_TAG, "prepare() failed")
-            }
-        }
-    }
-
-    private fun stopPlaying() {
-        player?.release()
-        player = null
-    }
-    private fun onRecord(start: Boolean) = if (start) {
-        startRecording()
-    } else {
-        stopRecording()
-    }
-
-    private fun startRecording() {
-        recorder = MediaRecorder().apply {
-            setAudioSource(MediaRecorder.AudioSource.MIC)
-            setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP)
-            setOutputFile(fileName)
-            setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB)
-
-            try {
-                prepare()
-            } catch (e: IOException) {
-                Log.e(LOG_TAG, "prepare() failed")
-            }
-
-            start()
+        binding.buttonStopRecord.setOnClickListener {
 
         }
     }
 
-    private fun stopRecording() {
-        recorder?.apply {
-            stop()
-            release()
-        }
-        recorder = null
-    }
 
-
-    internal inner class RecordButton(ctx: Context) :
-        androidx.appcompat.widget.AppCompatButton(ctx) {
-
-        var mStartRecording = true
-
-        var clicker: OnClickListener = OnClickListener {
-            onRecord(mStartRecording)
-            text = when (mStartRecording) {
-                true -> "Stop recording"
-                false -> "Start recording"
-            }
-            mStartRecording = !mStartRecording
-        }
-
-        init {
-            text = "Start recording"
-            setOnClickListener(clicker)
-        }
-    }
-
-    internal inner class PlayButton(ctx: Context) : androidx.appcompat.widget.AppCompatButton(ctx) {
-        var mStartPlaying = true
-        var clicker: OnClickListener = OnClickListener {
-            onPlay(mStartPlaying)
-            text = when (mStartPlaying) {
-                true -> "Stop playing"
-                false -> "Start playing"
-            }
-            mStartPlaying = !mStartPlaying
-        }
-
-        init {
-            text = "Start playing"
-            setOnClickListener(clicker)
-        }
-    }
-
-    fun create2() {
-
-        // Record to the external cache directory for visibility
-        fileName = "${externalCacheDir?.absolutePath}/audiorecordtest.3gp"
-
-        ActivityCompat.requestPermissions(this, permissions, REQUEST_RECORD_AUDIO_PERMISSION)
-
-        recordButton = RecordButton(this)
-        playButton = PlayButton(this)
-        val ll = LinearLayout(this).apply {
-            addView(
-                recordButton,
-                LinearLayout.LayoutParams(
-                    ViewGroup.LayoutParams.WRAP_CONTENT,
-                    ViewGroup.LayoutParams.WRAP_CONTENT,
-                    0f
-                )
-            )
-            addView(
-                playButton,
-                LinearLayout.LayoutParams(
-                    ViewGroup.LayoutParams.WRAP_CONTENT,
-                    ViewGroup.LayoutParams.WRAP_CONTENT,
-                    0f
-                )
-            )
-        }
-        setContentView(ll)
-    }
-
-    override fun onStop() {
-        super.onStop()
-        recorder?.release()
-        recorder = null
-        player?.release()
-        player = null
-    }
 }
