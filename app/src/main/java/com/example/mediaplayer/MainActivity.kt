3eead06552605861ee544a6b0f25d6e761925aa7
razi <rezaie127@yahoo.com>
2022-04-22 10:36:09 +0430

    change the music

--- app/src/main/java/com/example/mediaplayer/MainActivity.kt
+++ app/src/main/java/com/example/mediaplayer/MainActivity.kt
@@ -18,7 +18,7 @@ class MainActivity : AppCompatActivity() {
 
         binding.buttonPlay.setOnClickListener {
 
-            val url = "https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3" // your URL here
+            val url = "https://dl.niyazmusic.ir/Golchin/Best/Clip/128/music-clip%201%20%28NiyazMusic%29%28128%29.mp3"// your URL here
              mediaPlayer = MediaPlayer().apply {
                 setAudioAttributes(
                     AudioAttributes.Builder()
